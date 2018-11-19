04be515b33467936ca0aca9ad67db100d36ff9e1
ik013043z1 <ik013043z1@H013043.NET>
2018-11-19 11:37:56 +0100

    2.0

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -1,4 +1,30 @@
-
+import java.util.Scanner;
 public class PerfectNumber {
-
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first number");
+		int num1 = sc.nextInt();
+		System.out.println("Enter the first number");
+		int num2 = sc.nextInt();
+		System.out.println("Enter the first number");
+		int num3 = sc.nextInt();
+		System.out.println("Enter the first number");
+		int num4 = sc.nextInt();
+		System.out.println("Enter the first number");
+		int num5 = sc.nextInt();
+		System.out.println("Enter the first number");
+		int num6 = sc.nextInt();
+		System.out.println("Enter the first number");
+		int num7 = sc.nextInt();
+		System.out.println("Enter the first number");
+		int num8 = sc.nextInt();
+		System.out.println("Enter the first number");
+		int num9 = sc.nextInt();
+		System.out.println("Enter the first number");
+		int num10 = sc.nextInt();
+		
+		
+		
+		
+	}
 }
